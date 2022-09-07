8944d1a22828a3c383c491ef471d5459fff84e1a
Aleksey0810 <Aleksey.plotnichenko@gmail.com>
2022-09-07 22:46:54 +0300

    First commit

---
+++ src/ua/Plotnichenko/hw2/Task2.java
@@ -0,0 +1,22 @@
+package ua.Plotnichenko.hw2;
+
+import java.awt.*;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Point A = new Point(1, 5);
+        Point B = new Point(3, 5);
+        Point C = new Point(6, 7);
+        System.out.println("area of a triangle: "
+                + Triangle.area(A, B, C));
+    }
+}
+
+
+class Triangle {
+    public static float area(Point A, Point B, Point C) {
+        float area = (A.x * (B.y - C.y)
+                + B.x * (C.y - A.y) + C.x * (A.y - B.y)) / 2.0f;
+        return Math.abs(area);
+    }
+}
---
+++ src/ua/Plotnichenko/hw2/Task3.java
@@ -0,0 +1,19 @@
+package ua.Plotnichenko.hw2;
+
+public class Task3 {
+    public static void main(String[] args){
+
+        evenOrOdd(16);
+
+    }
+
+    public static void evenOrOdd(int number){
+
+        if (number % 2 == 0){
+            System.out.println("even number");
+        } else{
+            System.out.println("odd number");
+        }
+
+    }
+}
