28f180a532ebb78f592c1b55ec4633c3c5a85634
Aleksey0810 <Aleksey.plotnichenko@gmail.com>
2022-09-10 13:27:17 +0300

    hw3

---
+++ src/ua/Plotnichenko/hw3/Task3.java
@@ -0,0 +1,29 @@
+package ua.Plotnichenko.hw3;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Напишите что то :");
+        String ourText = scan.nextLine();
+        System.out.println("Текст до изменения : " + ourText);
+
+        wordsCalc(ourText);
+    }
+    public static int wordsCalc(String phrase) {
+        int count = 0;
+        if (phrase.length() != 0) {
+            count++;
+            for (int i = 0; i < phrase.length(); i++) {
+                if (phrase.charAt(i) == ' ') {
+                    count++;
+                }
+            }
+        }
+        System.out.println("В строке " + count + " слов ");
+
+        return count;
+    }
+}
+
