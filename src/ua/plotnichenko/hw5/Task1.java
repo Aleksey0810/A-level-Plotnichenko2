fd187211f49b8c9baafec635051a9538fc017c86
Aleksey0810 <Aleksey.plotnichenko@gmail.com>
2022-09-19 00:52:58 +0300

    hw5 fix

--- src/ua/plotnichenko/hw5/Task1.java
+++ src/ua/plotnichenko/hw5/Task1.java
@@ -4,44 +4,28 @@ import java.util.Arrays;
 
 public class Task1 {
     public static void main(String[] args) {
-        int[][] array = new int[4][3];
+        int[][] array = new int[4][4];
         fillArray(array);
-        printArray(array);
         System.out.println();
-        plusMinusString(array);
+
     }
 
     public static void fillArray(int[][] array) {
         int k = 0;
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
+            for (int j = 0; j < array.length; j++) {
                 array[i][j] = k + 1;
                 k++;
-            }
-        }
-    }
-
-    public static void printArray(int[][] array) {
-        for (int[] ints : array) {
-            for (int j = 0; j < 1; j++) {
-                System.out.println();
-            }
-            System.out.println(Arrays.toString(ints));
-        }
-    }
-
-    public static void plusMinusString(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (i % 2 != 0)
+                if (i % 2 != 0) {
                     array[i][j] = -1 * array[i][j];
+                }
             }
             System.out.println();
             System.out.print(Arrays.toString(array[i]));
+
         }
     }
 }
-
 
 
 
