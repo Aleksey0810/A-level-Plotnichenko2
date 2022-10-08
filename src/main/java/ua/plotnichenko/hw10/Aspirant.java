f80c1f9bdd1adcb2633e8a6407dfd1487c9545ee
Aleksey0810 <Aleksey.plotnichenko@gmail.com>
2022-10-09 00:07:05 +0300

    hw10

---
+++ src/main/java/ua/plotnichenko/hw10/Aspirant.java
@@ -0,0 +1,28 @@
+package ua.plotnichenko.hw10;
+
+public class Aspirant extends Student {
+    private String scientificWork;
+
+    public Aspirant(String firstName, String lastName, String group, double averageMark,
+                    String scientificWork) {
+        super(firstName, lastName, group, averageMark);
+        this.scientificWork = scientificWork;
+    }
+
+    public String getScientificWork() {
+        return scientificWork;
+    }
+
+    public void setScientificWork(String scientificWork) {
+        this.scientificWork = scientificWork;
+    }
+
+    public double getScholarship() {
+        if (getAverageMark() >= 5) {
+            return 200;
+        } else {
+            return 180;
+        }
+
+    }
+}
